41fca9a202f181557352310fa3e6fbffb5e3153a
Gustavo Leonildo de Souza <gustavoleonildodesouza@MacBook-Air-de-Gustavo.local>
2025-11-02 22:56:15 -0300

    feat:craete base service

---
+++ web/app/services/base.ts
@@ -0,0 +1,72 @@
+import { getToken } from './token'
+
+export class BaseApi {
+  protected baseUrl: string
+  protected token: string | undefined
+
+  constructor(baseUrl: string, token?: string) {
+    this.baseUrl = baseUrl
+    this.token = token
+  }
+
+  protected async get<T>(path: string): Promise<T> {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: 'GET',
+      headers: this.buildHeaders(),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Resquest GET failed with status ${response.status}`)
+    }
+
+    return response.json()
+  }
+
+  protected async post<T, R>(path: string, body?: T): Promise<R> {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: 'POST',
+      headers: this.buildHeaders(),
+      body: JSON.stringify(body),
+    })
+
+    if (!response.ok) {
+      throw new Error(`POST failed with status ${response.status}`)
+    }
+    return response.json()
+  }
+
+  protected async patch<T>(path: string, body?: T): Promise<T> {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: 'PATCH',
+      body: JSON.stringify(body),
+      headers: this.buildHeaders(),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Resquest PATCH failed with status ${response.status}`)
+    }
+
+    return response.json()
+  }
+
+  protected async delete<T>(path: string): Promise<T> {
+    const response = await fetch(`${this.baseUrl}${path}`, {
+      method: 'DELETE',
+      headers: this.buildHeaders(),
+    })
+
+    if (!response.ok) {
+      throw new Error(`Resquest DELETE failed with status ${response.status}`)
+    }
+
+    return response.json()
+  }
+
+  private buildHeaders() {
+    const token = getToken()
+    return {
+      'Content-Type': 'application/json',
+      ...(token && { Authorization: `Bearer ${token}` }),
+    }
+  }
+}
